bf9324c93d814eeafc5dc4d92f53ec057c2a3bad
Dinosaure <gweltaz27@hotmail.com>
2024-07-23 07:45:51 +0200

    WIP encore, Godot est sur la bonne lancée

--- Case.cs
+++ Case.cs
@@ -1,10 +1,8 @@
 using Godot;
 using System;
 
-public partial class MinesGridLayer : TileMapLayer
+public partial class Case : TextureButton
 {
-	public int cols = 2;
-	
 	// Called when the node enters the scene tree for the first time.
 	//public override void _Ready()
 	//{
@@ -14,4 +12,12 @@ public partial class MinesGridLayer : TileMapLayer
 	//public override void _Process(double delta)
 	//{
 	//}
+	
+	public override void _GuiInput(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mbe && mbe.ButtonIndex == MouseButton.Left && mbe.Pressed)
+		{
+			GD.Print("Left mouse button was pressed!");
+		}
+	}
 }
