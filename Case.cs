0eb119a64de9878c6c8426e56b39c51549bf3a95
Gweltaz <glemartret@hellowork.com>
2024-07-31 18:37:35 +0200

    WIP_Stabilisation + ajout Plateau et Case Reste à afficher les cases.

--- Case.cs
+++ Case.cs
@@ -1,23 +1,106 @@
-using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-public partial class Case : TextureButton
+//using System.Drawing.Drawing2D;
+
+//public class PixelBox : PictureBox
+//{
+//    protected override void OnPaint(PaintEventArgs pe)
+//    {
+//        pe.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+//        pe.Graphics.PixelOffsetMode = PixelOffsetMode.None;
+//        base.OnPaint(pe);
+//    }
+//}
+
+public class Case
 {
-	// Called when the node enters the scene tree for the first time.
-	//public override void _Ready()
-	//{
-	//}
+	private static int population = 0; //Déclaration d'existence dans la population
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	//public override void _Process(double delta)
+	public static int Size_x { get; set; } = 12 * 2;
+	public static int Size_y { get; set; } = 12 * 2;
+
+	//private static int nbVoisines = 0;
+	public bool isHidden; //La case n'est pas dévoilée
+	public bool isMined; //La case n'est pas minée (elle peut le devenir)
+	public bool isMarked; //La case n'est pas marquée comme minée
+	public int HasMineVoisines => Voisines.Count(c => c.isMined);
+	public List<Case> Voisines { get; set; } = [];
+
+	//public Image Image { get => (Image)pictBox.Image.Clone(); set { pictBox.Image = value; } }
+
+	private const string imgDir = @"Images\";
+	//private static readonly Image imgHidden = Image.FromFile(imgDir + "_.png");
+
+	////Affichage dans une PictureBox
+	//public PixelBox pictBox = new()
 	//{
-	//}
-	
-	public override void _GuiInput(InputEvent @event)
+	//    Size = new Size(Size_x, Size_y),  //dimensions
+	//    Name = population.ToString(), //nom
+	//    Image = imgHidden, //apparence
+	//    SizeMode = PictureBoxSizeMode.Zoom
+	//};
+
+	private Case? save;
+
+	public Case(bool _isMined = false, bool _isHidden = true, bool _isMarked = false/*int i_x, int i_y, Action<object?, EventArgs> pbCase_Click*/)
 	{
-		if (@event is InputEventMouseButton mbe && mbe.ButtonIndex == MouseButton.Left && mbe.Pressed)
-		{
-			GD.Print("Left mouse button was pressed!");
-		}
+		population++;
+		isHidden = _isHidden;
+		isMarked = _isMarked;
+		isMined = _isMined;
+		//Image = imgHidden;
+		//pictBox.Location = new Point(i_x * Size_x, i_y * Size_y); //positionnement du point haut-gauche
+		//pictBox.Click += new EventHandler(pbCase_Click);
+	}
+
+	public void Reveal()
+	{
+		//Image = !isMined ? Image.FromFile(imgDir + HasMineVoisines + ".png") : Image.FromFile(imgDir + "m.png");
+		isHidden = false;
+	}
+
+	public void Marque()
+	{
+		//Image = Image.FromFile(imgDir + "d.png");
+		isMarked = true;
+	}
+
+	public void Demarque()
+	{
+		//Image = imgHidden;
+		isMarked = false;
+	}
+
+	public void Restore() => Restore(save);
+
+	private void Restore(Case? @case)
+	{
+		if (@case is null) return;
+		isHidden = @case.isHidden;
+		isMined = @case.isMined;
+		isMarked = @case.isMarked;
+		//Image = @case.Image;
+	}
+
+	public void Save()
+	{
+		save = new(isMined, isHidden, isMarked);
+	}
+
+	public void Refresh()
+	{
+		if (isHidden) return;
+		//Image = !isMined ? Image.FromFile(imgDir + HasMineVoisines + ".png") : Image.FromFile(imgDir + "m.png");
+	}
+
+	internal void Démine()
+	{
+		isMined = false;
+		isMarked = false;
+		isHidden = false;
+		Refresh();
+		Voisines.ForEach(c => c.Refresh());
 	}
 }
--- FD_mineur.cs
+++ FD_mineur.cs
@@ -1,30 +1,63 @@
-#nullable enable
-
 using Godot;
 using System;
 
 public partial class FD_mineur : Control
 {
+	private class Re
+	{
+		protected Re()
+		{ }
+
+		public static Func<NodePath, Node> MaybeGetNode { protected get; set; } = _ => throw new NotImplementedException(nameof(MaybeGetNode));
+	}
+
+	private sealed class ReNode<TNode> : Re where TNode : Node
+	{
+		public ReNode(string path) : base()
+		{ _path = path; }
+
+		private readonly string _path;
+		private TNode? _me;
+		public TNode Me => _me ??= (TNode)MaybeGetNode(_path);
+	}
+
 	[Export]
 	public bool isGameOver = false;
-	private double elapsedTime = 0;
 
-	private Timer? timer;
-	private Label? tsslGameOver;
-	private Label? tsslTemps;
+	[Export]
+	public double elapsedTime = 0;
+
+	private readonly ReNode<Timer> timer = new("timer");
+	private readonly ReNode<Label> tsslGameOver = new("VBoxContainer/HBoxContainer/tsslGameOver");
+	private readonly ReNode<Label> tsslTemps = new("VBoxContainer/HBoxContainer/tsslTemps");
+
+	//private Label? _tsslReste; private Label? oldtsslReste => _tsslReste ??= (Label)GetNodeOrNull("VBoxContainer/HBoxContainer/tsslReste")
+	private readonly ReNode<Label> tsslReste = new("/root/FD_mineur/VBoxContainer/HBoxContainer/tsslReste");
+
+	private readonly ReNode<Label> tsslTotal = new("/root/FD_mineur/VBoxContainer/HBoxContainer/tsslTotal");
+	private readonly ReNode<ProgressBar> ProgressBar = new("/root/FD_mineur/VBoxContainer/HBoxContainer/ProgressBar");
+
+	public FD_mineur() : base()
+	{
+		Re.MaybeGetNode = GetNode;
+	}
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		//PrintTree();
-		timer = (Timer)GetNodeOrNull("timer");
-		timer.Start();
-		
-		tsslGameOver = (Label)GetNodeOrNull("VBoxContainer/HBoxContainer/tsslGameOver");
-		tsslGameOver.Visible = isGameOver;
-		
-		tsslTemps = (Label)GetNodeOrNull("VBoxContainer/HBoxContainer/tsslTemps");
-		tsslTemps.Text = FormatTime(elapsedTime);
+		tsslGameOver.Me.SetVisible(isGameOver);
+		tsslTemps.Me.SetText(FormatTime(elapsedTime));
+
+		Plateau.UpdateMines = (int min, int marques, int max) =>
+		{
+			tsslReste.Me.SetText((max - marques - min).ToString());
+			tsslTotal.Me.SetText(max.ToString());
+			ProgressBar.Me.SetMin(min);
+			ProgressBar.Me.SetValue(marques);
+			ProgressBar.Me.SetMax(max);
+		};
+		Plateau.InitialisePlateau();
+		timer.Me.Start();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,55 +66,58 @@ public partial class FD_mineur : Control
 		// Mettre à jour le temps écoulé
 		elapsedTime += delta;
 
-		tsslTemps.Text = FormatTime(elapsedTime);
+		tsslTemps.Me.SetText(isGameOver ? "(Temps écoulé)" : FormatTime(elapsedTime));
 	}
-	
+
 	public void SetGameOver(bool gameOver = true)
 	{
 		isGameOver = gameOver;
-		if (!(tsslGameOver is null)) tsslGameOver.Visible = gameOver;
+		tsslGameOver.Me.SetVisible(gameOver);
 	}
-	
-	private string FormatTime(double time)
+
+	private static string FormatTime(double time)
 	{
-		int heures = (int)time / 3600;
-		int minutes = (int)(time % 3600) / 60;
-		double secondes = (double)time % 60;
-		return string.Format("{0:00}:{1:00}", minutes, secondes);
+		var (heures, reste) = GetDivMod(time, 3600);
+		var (minutes, secondes) = GetDivMod(reste, 60);
+		return $"{heures:00}:{minutes:00}:{secondes:00}";
 	}
-	
-	private static (int entier, double reste) GetDivMod(double time, double baseValue, bool forceSign = false)
+
+	private static (int entier, double reste) GetDivMod(double valeur, double baseValeur, bool forceSign = false)
 	{
-		double reste = time;
+		double reste = valeur;
 		int entier = 0;
-		if (baseValue > 0)
+		switch (baseValeur)
 		{
-			while (reste >= baseValue)
-			{
-				entier++;
-				reste -= baseValue;
-			}
-			while (forceSign && reste < 0)
-			{
-				entier--;
-				reste += baseValue;
-			}
+			case > 0:
+				while (reste >= baseValeur)
+				{
+					entier++;
+					reste -= baseValeur;
+				}
+				while (forceSign && reste < 0)
+				{
+					entier--;
+					reste += baseValeur;
+				}
+				break;
+
+			case < 0:
+				while (reste <= baseValeur)
+				{
+					entier++;
+					reste += baseValeur;
+				}
+				while (forceSign && reste > 0)
+				{
+					entier--;
+					reste -= baseValeur;
+				}
+				break;
+
+			default:
+				break;
 		}
-		else if (baseValue < 0)
-		{
-			while (reste <= baseValue)
-			{
-				entier++;
-				reste += baseValue;
-			}
-			while (forceSign && reste > 0)
-			{
-				entier--;
-				reste -= baseValue;
-			}
-		}
-		//else noop;
-		
+
 		return (entier, reste);
 	}
 }
